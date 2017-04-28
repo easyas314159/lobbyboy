c1e88a17d19772e79f65999f4d554aa37dc82650
Kevin Loney <kevin.loney@brainsinjars.com>
2017-04-28 02:23:50 -0600

    terraform fmt

--- terraform/encrypt/main.tf
+++ terraform/encrypt/main.tf
@@ -6,16 +6,16 @@ variable "plain_text" {}
 variable "output_file" {}
 
 resource "null_resource" "encrypted" {
-	triggers = {
-		key_id = "${var.key_id}"
-		plain_text = "${var.plain_text}"
-	}
+  triggers = {
+    key_id     = "${var.key_id}"
+    plain_text = "${var.plain_text}"
+  }
 
-	provisioner "local-exec" {
-		command = "aws kms encrypt --key-id ${var.key_id} --plaintext \"${var.plain_text}\" --output text --query CiphertextBlob --profile \"${var.profile}\" --region \"${var.region}\" > ${var.output_file}"
-	}
+  provisioner "local-exec" {
+    command = "aws kms encrypt --key-id ${var.key_id} --plaintext \"${var.plain_text}\" --output text --query CiphertextBlob --profile \"${var.profile}\" --region \"${var.region}\" > ${var.output_file}"
+  }
 }
 
 output "encrypted" {
-	value = "${file(var.output_file)}"
+  value = "${file(var.output_file)}"
 }
