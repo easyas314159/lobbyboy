c2ad102f56fd8e464c6df026391d51c6ce219ba9
easyas314159 <kevin.loney@brainsinjars.com>
2021-08-15 01:58:40 -0600

    Got twilio incoming request validation working

--- go/cmd/lobbyboy/routes.go
+++ go/cmd/lobbyboy/routes.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/BTBurke/twiml"
+	"github.com/awslabs/aws-lambda-go-api-proxy/core"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	twilio "github.com/kevinburke/twilio-go"
@@ -29,8 +31,8 @@ func (env *Environment) createRouter() *mux.Router {
 			return handlers.CompressHandlerLevel(next, 7)
 		},
 		handlers.RecoveryHandler(),
-		// env.twilioValidation,
 		env.twilioVoiceRequest,
+		env.twilioValidation,
 	)
 
 	r.Path("/menu").Methods("POST").HandlerFunc(env.handleMenu)
@@ -43,10 +45,32 @@ func (env *Environment) createRouter() *mux.Router {
 
 func (env *Environment) twilioValidation(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := twilio.ValidateIncomingRequest("", "", r)
-		if err != nil {
-			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-			return
+		if env.Config.IsSet("twilio.secret") {
+			twilioSecret := env.Config.GetString("twilio.secret")
+
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+				log.Printf("Failed to parse form: %v\n", err)
+				return
+			}
+
+			url := r.URL.String()
+			apiGatewayContext, exists := core.GetAPIGatewayContextFromContext(r.Context())
+			if exists {
+				tmpURL := *r.URL
+				tmpURL.Path = fmt.Sprintf("/%s%s", apiGatewayContext.Stage, tmpURL.Path)
+				url = tmpURL.String()
+			}
+
+			expected := r.Header.Get("X-Twilio-Signature")
+			actual := twilio.GetExpectedTwilioSignature("", twilioSecret, url, r.PostForm)
+
+			if expected != actual {
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+				log.Println("Twilio request validation failure.")
+				return
+			}
 		}
 
 		next.ServeHTTP(w, r)
