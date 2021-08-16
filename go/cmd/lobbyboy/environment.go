0847651ba3ce651c5869a88f0e94cf8bc928dc94
easyas314159 <kevin.loney@brainsinjars.com>
2021-08-16 12:49:28 -0600

    Started populating the phone number directory into the environment from the config source

--- go/cmd/lobbyboy/environment.go
+++ go/cmd/lobbyboy/environment.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"log"
+
 	"github.com/BTBurke/twiml"
 	"github.com/spf13/viper"
 )
 
 type Environment struct {
-	Config *viper.Viper
+	Config    *viper.Viper
+	Directory *Directory
 }
 
 func NewEnvironment(cfg *viper.Viper) (*Environment, error) {
+	d := NewDirectory()
+	cfgDir := cfg.Sub("directory")
+	for _, name := range cfgDir.AllKeys() {
+		numbers := cfgDir.GetStringSlice(name)
+		for _, number := range numbers {
+			err := d.Add(name, number)
+			if err != nil {
+				log.Printf("%s: %v\n", name, err)
+			}
+			continue
+		}
+	}
+
 	return &Environment{
-		Config: cfg,
+		Config:    cfg,
+		Directory: d,
 	}, nil
 }
 
